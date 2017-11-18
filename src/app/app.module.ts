30ccaeb356629b1df4ca906879afaf5c17efc5ab
azenakhi <azenakhi@ahmeds-mbp.home>
2017-11-18 11:33:12 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TestComponent } from './test/test.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TestComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
